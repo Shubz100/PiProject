08d2c32106f6905b48e00e92e7dd45d63b6fa234
Shubz100 <115923262+Shubz100@users.noreply.github.com>
2024-10-21 00:57:28 +0530

    Update and rename page.ts to page.tsx

---
+++ fonts/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React, { useState } from 'react';
+
+function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    )
+  export default App;
